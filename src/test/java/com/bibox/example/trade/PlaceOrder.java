c909807ede5c6d51c629b253d7e53081408369dd
BiBox.com <33807615+Biboxcom@users.noreply.github.com>
2020-12-21 13:46:54 +0800

    Update PlaceOrder.java

--- src/test/java/com/bibox/example/trade/PlaceOrder.java
+++ src/test/java/com/bibox/example/trade/PlaceOrder.java
@@ -14,7 +14,7 @@ public class PlaceOrder {
         String apiKey = "adc88f86bfa5598ae76b2e512c123b9cac5f4ac8";
         String secretKey = "12afe3046e782c85066d5da2b533036a981efbff";
         BiboxFuturesClient client = new BiboxFuturesClient(apiKey, secretKey);
-        // placeLimitOrder(client);
+        placeLimitOrder(client);
         placeMarketOrder(client);
     }
 
